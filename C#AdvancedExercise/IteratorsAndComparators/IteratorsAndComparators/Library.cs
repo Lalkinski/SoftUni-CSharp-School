4cfa3fc5c91791403765817c76b15f7e0110c780
Lalkinski <156706356+Lalkinski@users.noreply.github.com>
2024-06-16 17:31:27 +0300

    Add files via upload

---
+++ C#AdvancedExercise/IteratorsAndComparators/IteratorsAndComparators/Library.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        private List<Book> books;
+
+        public Library(params Book[] books)
+        {
+            this.books = books.ToList();
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return new LibraryIterator(books.OrderBy(b => b, new BookComparator()).ToList());
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        private class LibraryIterator : IEnumerator<Book>
+        {
+            int currentIndex = -1;
+            private List<Book> books;
+
+            public LibraryIterator(List<Book> books)
+            {
+                this.books = books;
+            }
+
+            public Book Current => books[this.currentIndex];
+
+            object IEnumerator.Current => this.currentIndex;
+
+            public void Dispose() { }
+
+            public bool MoveNext()
+            {
+                return ++this.currentIndex < this.books.Count;
+            }
+
+            public void Reset()
+            {
+                currentIndex = -1;
+            }
+        }
+
+    }
+
+}
