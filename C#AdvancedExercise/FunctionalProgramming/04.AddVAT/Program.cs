510fd51501ab317032578c923adcb9e97bda168f
Lalkinski <156706356+Lalkinski@users.noreply.github.com>
2024-06-16 17:26:36 +0300

    Add files via upload

---
+++ C#AdvancedExercise/FunctionalProgramming/04.AddVAT/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+1.38, 2.56, 4.4
+
+1, 3, 5, 7
+ */
+class Program
+{
+    static void Main()
+    {
+        double[] prices = Console.ReadLine()
+            .Split(new string[] {", "}, StringSplitOptions.RemoveEmptyEntries)
+            .Select(double.Parse)
+            .Select(n => n * 1.2)
+            .ToArray();
+
+        foreach(var price in prices)
+        {
+            Console.WriteLine($"{price:f2}");
+        }
+    }
+}
+
