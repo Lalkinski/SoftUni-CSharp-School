02f45effb37c9f5d844cad850871145721470b17
Lalkinski <156706356+Lalkinski@users.noreply.github.com>
2024-06-16 23:05:48 +0300

    Add files via upload

---
+++ C#Fundamentals/FundamentalsLab/ArraysLab/07. Equal Arrays/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _07._Equal_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] firstArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] secondArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            for (int i = 0; i < firstArray.Length; i++)
+            {
+                if (firstArray[i] != secondArray[i])
+                {
+                    Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
+                    return;
+                }
+            }
+
+            int sum = 0;
+
+            foreach (int currentNumber in firstArray)
+            {
+                sum += currentNumber;   
+            }
+
+            Console.WriteLine($"Arrays are identical. Sum: {sum}");
+        }
+    }
+}
