26bbe5ad6a470afa87b6eff966fc6c8fcdea9b46
Lalkinski <156706356+Lalkinski@users.noreply.github.com>
2024-06-16 17:25:03 +0300

    Add files via upload

---
+++ C#AdvancedExercise/FunctionalProgramming/01.SortEvenNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+4, 2, 1, 3, 5, 7, 1, 4, 2, 12
+
+1, 3, 5
+
+2, 4, 6
+ */
+class Program
+{
+    static void Main()
+    {
+        List<int> numbers = Console.ReadLine()
+            .Split(", ")
+            .Select(int.Parse)
+            .Where(x => x % 2 == 0)
+            .OrderBy(x => x)
+            .ToList();
+
+        Console.WriteLine(string.Join(", ", numbers));
+    }
+}
+
