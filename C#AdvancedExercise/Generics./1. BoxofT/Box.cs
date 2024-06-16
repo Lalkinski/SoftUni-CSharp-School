07a28f5a205e9bb5664b5f93d30c8ef241d121c8
Lalkinski <156706356+Lalkinski@users.noreply.github.com>
2024-06-16 17:28:38 +0300

    Add files via upload

---
+++ C#AdvancedExercise/Generics./1. BoxofT/Box.cs
@@ -0,0 +1,24 @@
+﻿namespace BoxofT
+{
+    public class Box<T>
+    {
+        private Stack<T> box;
+
+        public Box()
+        {
+            box = new Stack<T>();
+        }
+
+        public void Add(T item)
+        {
+            box.Push(item);
+        }
+
+        public T Remove()
+        {
+            return box.Pop();
+        }
+
+        public int Count { get { return box.Count; } }
+    }
+}
