b9f71e8f9fb7ac15d8b3cf14f849e3f1ead3b413
Lalkinski <156706356+Lalkinski@users.noreply.github.com>
2024-06-16 17:30:12 +0300

    Add files via upload

---
+++ C#AdvancedExercise/Generics./GenericScale/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace GenericScale
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EqualityScale<int> scale = new EqualityScale<int>(6, 6);
+
+            Console.WriteLine(scale.AreEqual());
+        }
+    }
+}
