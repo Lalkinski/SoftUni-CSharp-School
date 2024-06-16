47958c40ae4991203eadbe3faddc52271ff696e2
Lalkinski <156706356+Lalkinski@users.noreply.github.com>
2024-06-16 17:35:37 +0300

    Add files via upload

---
+++ C#AdvancedExercise/SetsAndDictionariesAdvancedLab/01.CountSameValuesInArray/Program.cs
@@ -0,0 +1,26 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Dictionary<string, int> keyValuePairs = new Dictionary<string, int>();
+
+        string[] input = Console.ReadLine().Split();
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            if (!keyValuePairs.ContainsKey(input[i]))
+            {
+                keyValuePairs.Add(input[i], 0);
+            }
+
+            keyValuePairs[input[i]]++;
+        }
+
+        foreach (var item in keyValuePairs)
+        {
+            Console.WriteLine($"{item.Key} - {item.Value} times");
+        }
+
+    }
+}
+
