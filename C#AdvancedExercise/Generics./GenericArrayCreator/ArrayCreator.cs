e682beee63b85c4da6503e1d47ecca5c87729dff
Lalkinski <156706356+Lalkinski@users.noreply.github.com>
2024-06-16 17:29:33 +0300

    Add files via upload

---
+++ C#AdvancedExercise/Generics./GenericArrayCreator/ArrayCreator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericArrayCreator
+{
+    public class ArrayCreator
+    {
+        
+        public static T[] Create<T>(int lenght, T item)
+        {
+            T[] array = new T[lenght];
+
+            for (int i = 0; i < lenght; i++)
+            {
+                array[i] = item;
+            }
+
+            return array;   
+        }
+    }
+}
