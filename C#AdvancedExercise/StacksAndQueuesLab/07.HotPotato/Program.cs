c6d7e082ccd09a85e985fceaf4aff1d47f58f28c
Lalkinski <156706356+Lalkinski@users.noreply.github.com>
2024-06-16 22:31:13 +0300

    Add files via upload

---
+++ C#AdvancedExercise/StacksAndQueuesLab/07.HotPotato/Program.cs
@@ -0,0 +1,28 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Queue<string> queue = new Queue<string>(Console.ReadLine().Split(' '));
+        int n = int.Parse(Console.ReadLine());
+
+        int moves = 1;
+        while(queue.Count != 1)
+        {
+            string childToPlay = queue.Dequeue();
+            if(moves == n)
+            {
+                Console.WriteLine($"Removed {childToPlay}");
+                moves = 0;
+            }
+            else
+            {
+                queue.Enqueue(childToPlay);
+            }
+
+            moves++;
+        }
+
+        Console.WriteLine($"Last is {queue.Dequeue()}");
+    }
+}
+
