186369f4c92bf72e9f16a48855de6f79e7acbf49
Lalkinski <156706356+Lalkinski@users.noreply.github.com>
2024-06-16 17:25:33 +0300

    Add files via upload

---
+++ C#AdvancedExercise/FunctionalProgramming/02.SumNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+4, 2, 1, 3, 5, 7, 1, 4, 2, 12
+
+2, 4, 6
+ */
+class Program
+{
+    static void Main()
+    {
+        List<int> numbers = Console.ReadLine()
+            .Split(", ")
+            .Select(int.Parse)
+            .ToList();
+
+        int sum = numbers.Sum();
+
+        Console.WriteLine(numbers.Count);
+        Console.WriteLine(sum);
+    }
+}
+
