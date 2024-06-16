4a10fdb30e479921fdfcfa25fc49cc3d21be292d
Lalkinski <156706356+Lalkinski@users.noreply.github.com>
2024-06-16 22:46:41 +0300

    Add files via upload

---
+++ C#AdvancedExercise/FunctionalProgrammingExercise/11.TriFunction/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int asciiSum = int.Parse(Console.ReadLine());
+        string[] names = Console.ReadLine()
+            .Split();
+
+        Func<string, int, bool> filter = (name, sum)
+            => name.Sum(x => x) >= sum;
+
+        Func<string[], Func<string, int, bool>, string> filterFirstName
+            = (allNames, funcFilter)
+            => allNames.FirstOrDefault(x => funcFilter(x, asciiSum));
+
+        Console.WriteLine(filterFirstName(names, filter));
+    }
+}
