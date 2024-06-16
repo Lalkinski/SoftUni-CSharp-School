fc4713c8ee3d9d1fe3ab519b5342cdfd92ff89bd
Lalkinski <156706356+Lalkinski@users.noreply.github.com>
2024-06-16 22:39:55 +0300

    Add files via upload

---
+++ C#AdvancedExercise/CreateCustomDataStructur/CostumeList/CustomList.cs
@@ -0,0 +1,15 @@
+﻿
+namespace CostumeList
+{
+    public class CustomList
+    {
+        private const int InitialCapacity = 2;
+        private int[] items;
+
+        int couter = 0;
+        public void Add(int value)
+        {
+            this.items[couter++] = value;
+        }
+    }
+}
