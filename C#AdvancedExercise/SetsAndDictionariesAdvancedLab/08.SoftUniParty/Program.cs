4826bacc353cf164a3c2df1d535ac5cf8c23018b
Lalkinski <156706356+Lalkinski@users.noreply.github.com>
2024-06-16 17:38:20 +0300

    Add files via upload

---
+++ C#AdvancedExercise/SetsAndDictionariesAdvancedLab/08.SoftUniParty/Program.cs
@@ -0,0 +1,57 @@
+﻿/*
+7IK9Yo0h
+9NoBUajQ
+Ce8vwPmE
+SVQXQCbc
+tSzE5t0p
+PARTY
+9NoBUajQ
+Ce8vwPmE
+SVQXQCbc
+END */
+class Program
+{
+    static void Main()
+    {
+        HashSet<string> vipGuest = new HashSet<string>();
+        HashSet<string> regularGuests = new HashSet<string>();
+
+        string input;
+        while((input = Console.ReadLine()) != "PARTY")
+        {
+            if (char.IsDigit(input[0]))
+            {
+                vipGuest.Add(input);
+            }
+            else
+            {
+                regularGuests.Add(input);
+            }
+        }
+
+        while((input = Console.ReadLine()) != "END")
+        {
+            if(vipGuest.Contains(input))
+            {
+                vipGuest.Remove(input);
+            }
+            else if(regularGuests.Contains(input))
+            {
+                regularGuests.Remove(input);
+            }
+        }
+
+        Console.WriteLine(vipGuest.Count + regularGuests.Count);
+
+        foreach(var guest in vipGuest)
+        {
+            Console.WriteLine(guest);
+        }
+
+        foreach(var guest in regularGuests)
+        {
+            Console.WriteLine(guest);
+        }
+    }
+}
+
