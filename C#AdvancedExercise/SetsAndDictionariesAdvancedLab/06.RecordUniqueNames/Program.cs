f61d10ec8258f521c88c52b1eb88e58c6a812697
Lalkinski <156706356+Lalkinski@users.noreply.github.com>
2024-06-16 17:37:37 +0300

    Add files via upload

---
+++ C#AdvancedExercise/SetsAndDictionariesAdvancedLab/06.RecordUniqueNames/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+8
+John
+Alex
+John
+Sam
+Alex
+Alice
+Peter 
+Alex
+ */
+class Program
+{
+    static void Main()
+    {
+        HashSet<string> set = new HashSet<string>();
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 0; i < n; i++)
+        {
+            set.Add(Console.ReadLine());
+        }
+
+        Console.WriteLine(string.Join("\n", set));
+    }
+}
+
