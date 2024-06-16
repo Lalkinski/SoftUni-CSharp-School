7d70810221b0c1cf1f64ea0827961acb82e7f27b
Lalkinski <156706356+Lalkinski@users.noreply.github.com>
2024-06-16 17:36:24 +0300

    Add files via upload

---
+++ C#AdvancedExercise/SetsAndDictionariesAdvancedLab/03.Largest3Numbers/Program.cs
@@ -0,0 +1,12 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray(); 
+
+        int[] sorted = numbers.OrderByDescending(x => x).ToArray();
+
+        Console.Write(string.Join(" ", sorted.Take(3)));
+    }
+}
+
