dabcea2871032863103b494c41a11d15e2afaa9c
Lalkinski <156706356+Lalkinski@users.noreply.github.com>
2024-06-17 23:18:09 +0300

    Add files via upload

---
+++ C#Fundamentals/FundamentalsLab/DictionariesLambdaAndLINQLab/1. Count Real Number/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _1._Count_Real_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = Console.ReadLine()
+                .Split()
+                .Select(double.Parse)
+                .ToArray();
+
+            SortedDictionary<double, int> counts = new SortedDictionary<double, int>();
+
+            foreach (var number in numbers)
+            {
+                if(counts.ContainsKey(number))
+                {
+                    counts[number]++;
+                }
+                else
+                {
+                    counts.Add(number, 1);
+                }
+            }
+
+            foreach (var number in counts)
+            {
+                Console.WriteLine($"{number.Key} -> {number.Value}");
+            }
+        }
+    }
+}
---
+++ C#Fundamentals/FundamentalsLab/DictionariesLambdaAndLINQLab/3. Word/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _3._Word
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<string>> result = new Dictionary<string, List<string>>();   
+
+            for (int i = 0; i < n; i++)
+            {
+                string word = Console.ReadLine();
+                string synonym = Console.ReadLine();
+
+                if (!result.ContainsKey(word))
+                {
+                    result[word] = new List<string>();
+                }
+
+                result[word].Add(synonym);
+            }
+
+            foreach(KeyValuePair<string, List<string>> kvp in result)
+            {
+                Console.WriteLine($"{kvp.Key} - {string.Join(", ", kvp.Value)}");
+            }
+        }
+    }
+}
