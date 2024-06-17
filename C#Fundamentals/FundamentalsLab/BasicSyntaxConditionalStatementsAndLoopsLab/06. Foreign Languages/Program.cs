78016e1fbbc09bbc60dca1e062f20b9c7b13a19f
Lalkinski <156706356+Lalkinski@users.noreply.github.com>
2024-06-17 23:08:14 +0300

    Add files via upload

---
+++ C#Fundamentals/FundamentalsLab/BasicSyntaxConditionalStatementsAndLoopsLab/06. Foreign Languages/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _06._Foreign_Languages
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string country = Console.ReadLine();
+
+           if(country == "USA" || country == "England")
+            {
+                Console.WriteLine("English");
+            }
+           else if (country == "Spain" || country == "Argentina" || country == "Mexico")
+            {
+                Console.WriteLine("Spanish");
+            }
+           else
+            {
+                Console.WriteLine("unknown");
+            }
+        }
+    }
+}
