304398f8de61dd6bfe96a2f8378cf72f55df54b0
Lalkinski <156706356+Lalkinski@users.noreply.github.com>
2024-06-17 23:21:02 +0300

    Add files via upload

---
+++ C#Fundamentals/FundamentalsLab/ListsLab/04. List of Products/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _4._List_of_Products
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<string> list = new List<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string product = Console.ReadLine();
+
+                 list.Add(product);
+                 list.Sort();
+            }
+
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine($"{i+1}.{list[i]}");
+            }
+        }
+    }
+}
---
+++ C#Fundamentals/FundamentalsLab/ListsLab/06. List Manipulation Basics/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _06._List_Manipulation_Basics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            string line;
+            while((line = Console.ReadLine()) != "end")
+            {
+                string[] token = line.Split();
+
+                string command = token[0];
+                
+
+                if(command == "Add")
+                {
+                    list.Add(int.Parse(token[1]));
+                }
+                else if(command == "Remove")
+                {
+                    list.Remove(int.Parse(token[1]));
+                }
+                else if(command == "Insert")
+                {
+                    int index = int.Parse(token[2]);
+                    int number = int.Parse(token[1]);
+                    list.Insert(index, number);
+                }
+                else
+                {
+                    int index = int.Parse(token[1]);
+                    list.RemoveAt(index);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", list));
+        }
+    }
+}
