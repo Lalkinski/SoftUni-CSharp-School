096dd8e5e5c64edb960582af8662b836dd6b4161
Lalkinski <156706356+Lalkinski@users.noreply.github.com>
2024-06-17 23:10:45 +0300

    Add files via upload

---
+++ C#Fundamentals/FundamentalsLab/BasicSyntaxConditionalStatementsAndLoopsLab/13. Holidays Between Two Dates/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+class HolidaysBetweenTwoDates
+{
+    static void Main(string[] args)
+    {
+        var StartDate = DateTime.ParseExact(Console.ReadLine(), 
+            "d.M.yyyy", CultureInfo.InvariantCulture);
+        var endDate = DateTime.ParseExact(Console.ReadLine(),
+            "d.M.yyyy", CultureInfo.InvariantCulture );
+
+        var holidaysCount = 0;
+        for(var date = StartDate; date <= endDate; date = date.AddDays(1))
+        {
+            if(date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday)
+            {
+                holidaysCount++;
+            }
+        }
+
+        Console.WriteLine(holidaysCount);
+    }
+}
