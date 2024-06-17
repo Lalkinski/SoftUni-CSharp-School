c8391b87b15b242f4ecad9a82fa654ed95d6112a
Lalkinski <156706356+Lalkinski@users.noreply.github.com>
2024-06-17 23:15:49 +0300

    Add files via upload

---
+++ C#Fundamentals/FundamentalsLab/DataTypesAndVariablesLab/09. Chars to String/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _09._Chars_to_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string one = Console.ReadLine();
+            string two = Console.ReadLine();
+            string three = Console.ReadLine();
+
+            Console.WriteLine($"{one}{two}{three}");
+        }
+    }
+}
---
+++ C#Fundamentals/FundamentalsLab/DataTypesAndVariablesLab/10. Lower or Upper/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        char inputChar;
+
+        inputChar = Console.ReadLine()[0];
+
+        if (char.IsUpper(inputChar))
+        {
+            Console.WriteLine("\nupper-case");
+        }
+        else if (char.IsLower(inputChar))
+        {
+            Console.WriteLine("\nlower-case");
+        }
+       
+    }
+}
