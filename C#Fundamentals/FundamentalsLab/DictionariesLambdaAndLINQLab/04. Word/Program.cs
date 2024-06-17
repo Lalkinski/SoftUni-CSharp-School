65ba4992c9e57bbdf762b02f2ec0c67ceed1fc00
Lalkinski <156706356+Lalkinski@users.noreply.github.com>
2024-06-17 23:19:01 +0300

    Add files via upload

---
+++ C#Fundamentals/FundamentalsLab/DictionariesLambdaAndLINQLab/04. Word/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _04._Word
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine()
+                .Split()
+                .Where(s => s.Length % 2 == 0)
+                .ToArray();
+
+            foreach (string word in words)
+            {
+                Console.Write(word);
+                Console.WriteLine();
+            }
+        }
+    }
+}
