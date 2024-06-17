928a88a3e499f6a98191b1cb2be5735a2303d691
Lalkinski <156706356+Lalkinski@users.noreply.github.com>
2024-06-17 23:09:07 +0300

    Add files via upload

---
+++ C#Fundamentals/FundamentalsLab/BasicSyntaxConditionalStatementsAndLoopsLab/07. Theatre Promotion/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Diagnostics;
+
+namespace _07._Theatre_Promotion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string day = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            decimal sum = 0;
+
+            if (age < 0)
+            {
+                Console.WriteLine("Error!");
+                return;
+            }
+
+            if (day == "Weekday")
+            {
+                if((age >= 0 && age <= 18) ||  (age > 64 && age <= 122))
+                {
+                    sum = 12;
+                }
+                else if(age > 18 && age <= 64)
+                {
+                    sum = 18;
+                }
+            }
+            else if (day == "Weekend")
+            {
+                if ((age >= 0 && age <= 18) || (age > 64 && age <= 122))
+                {
+                    sum = 15;
+                }
+                else if (age > 18 && age <= 64)
+                {
+                    sum = 20;
+                }
+            }
+            else if (day == "Holiday")
+            {
+                if (age >= 0 && age <= 18)
+                {
+                    sum = 5;
+                }
+                else if (age > 18 && age <= 64)
+                {
+                    sum = 12;
+                }
+                else if(age > 64 && age <= 122)
+                {
+                    sum = 10;
+                }
+            }
+
+            if (sum != 0)
+            {
+                Console.WriteLine($"{sum}$");
+            }
+            else
+            {
+                Console.WriteLine("Error!");
+            };
+        }
+    }
+}
---
+++ C#Fundamentals/FundamentalsLab/BasicSyntaxConditionalStatementsAndLoopsLab/09. Sum of Odd Numbers/Program.cs
@@ -0,0 +1,53 @@
+﻿//namespace _09._Sum_of_Odd_Numbers
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            int number = int.Parse(Console.ReadLine());
+//            int sum = 0;
+//            if (number < 1 || number > 100)
+//            {
+//                Console.WriteLine("Invalid input. n should be in the interval [1…100].");
+//                return;
+//            }
+
+//            for (int i = 1; i <= number; i++)
+//            {
+//                int oddNUmber = 2 * 1 - 1;
+//                Console.WriteLine(oddNUmber);
+//                sum += oddNUmber;
+//            }
+
+//            Console.WriteLine($"Sum: {sum}");
+//        }
+//    }
+//}
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+        int n = int.Parse(Console.ReadLine());
+
+        if (n < 1 || n > 100)
+        {
+           
+            return;
+        }
+
+       
+        int sum = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            int oddNumber = 2 * i - 1;
+            Console.WriteLine(oddNumber);
+            sum += oddNumber;
+        }
+
+       
+        Console.WriteLine($"Sum: {sum}");
+    }
+}
