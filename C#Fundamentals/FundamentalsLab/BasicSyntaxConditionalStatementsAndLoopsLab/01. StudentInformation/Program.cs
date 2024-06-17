90d8f52b5ec48972a20121ad180e73fb7ba9b1b8
Lalkinski <156706356+Lalkinski@users.noreply.github.com>
2024-06-17 23:07:24 +0300

    Add files via upload

---
+++ C#Fundamentals/FundamentalsLab/BasicSyntaxConditionalStatementsAndLoopsLab/01. StudentInformation/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _1._1._Student_Information
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            double averageGrade = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Name: {name}, Age: {age}, Grade: {averageGrade:f2}");
+        }
+    }
+}
---
+++ C#Fundamentals/FundamentalsLab/BasicSyntaxConditionalStatementsAndLoopsLab/03. Passed or Failed/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _02._Passed
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double grade = double.Parse(Console.ReadLine());
+
+            if (grade >= 3.00)
+            {
+                Console.WriteLine("Passed!");
+            }
+            else if(grade < 3.00)
+            {
+                Console.WriteLine("Failed!");
+            }
+        }
+   }
+}
