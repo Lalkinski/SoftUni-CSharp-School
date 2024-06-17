cff16e8a2d7e1861b6e4e584aa296a8742dd360f
Lalkinski <156706356+Lalkinski@users.noreply.github.com>
2024-06-17 23:14:54 +0300

    Add files via upload

---
+++ C#Fundamentals/FundamentalsLab/DataTypesAndVariablesLab/05.SpecialNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class SpecialNumberProgram
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= n; i++)
+        {
+            bool isSpecial = CheckSpecialNumber(i);
+            Console.WriteLine(i + " -> " + isSpecial);
+        }
+    }
+
+    static bool CheckSpecialNumber(int num)
+    {
+        int sumOfDigits = 0;
+
+        int tempNum = num;
+        while (tempNum > 0)
+        {
+            int lastDigit = tempNum % 10;
+            sumOfDigits += lastDigit;
+            tempNum /= 10;
+        }
+
+        return (sumOfDigits == 5 || sumOfDigits == 7 || sumOfDigits == 11);
+    }
+}
---
+++ C#Fundamentals/FundamentalsLab/DataTypesAndVariablesLab/07. Concat Names/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _06._Reversed_Chars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstLine = Console.ReadLine();
+            string secondLine = Console.ReadLine();
+            string thirdLine = Console.ReadLine();
+
+            Console.Write($"{firstLine}{thirdLine}{secondLine}");
+        }
+    }
+}
