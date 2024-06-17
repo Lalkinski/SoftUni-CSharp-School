6956f2242548f22d97a2cfd844176be6ddc41595
Lalkinski <156706356+Lalkinski@users.noreply.github.com>
2024-06-17 23:16:59 +0300

    Add files via upload

---
+++ C#Fundamentals/FundamentalsLab/DataTypesAndVariablesLab/11. Refactor Volume of Pyramid/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+2.5 
+4.2
+5.3
+
+
+// Function to find the  
+   // volume of square pyramid 
+   float volumeSquare(int b, int h) 
+   { 
+       float vol = (0.33) * b *  
+                        b * h; 
+       return vol; 
+   } 
+
+ */
+
+namespace _11._Refactor_Volume_of_Pyramid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Length: ");
+            decimal length = decimal.Parse(Console.ReadLine());
+
+            Console.Write("Width: ");
+            decimal width = decimal.Parse(Console.ReadLine());
+
+            Console.Write("Height: ");
+            decimal height = decimal.Parse(Console.ReadLine());
+
+            height = (length * width * height) / 3;
+            Console.Write($"Pyramid Volume: {height:f2}");
+        }
+    }
+}
---
+++ C#Fundamentals/FundamentalsLab/DataTypesAndVariablesLab/12. Refactor Special Numbers/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _12._Refactor_Special_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int i = 0;
+            bool isSpecialNum = false;
+            for (int ch = 1; ch <= count; ch++)
+            {
+                i = ch;
+                while (ch > 0)
+                {
+                    sum += ch % 10;
+                    ch = ch / 10;
+                }
+
+                isSpecialNum = (sum == 5) || (sum == 7) || (sum == 11);
+                Console.WriteLine("{0} -> {1}", i, isSpecialNum);
+                sum = 0;
+                ch = i;
+            }
+        }
+    }
+}
