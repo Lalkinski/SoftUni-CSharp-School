4a5ca6728141557fa514309c457bf5a961fec2c5
Lalkinski <156706356+Lalkinski@users.noreply.github.com>
2024-06-17 23:14:05 +0300

    Add files via upload

---
+++ C#Fundamentals/FundamentalsLab/DataTypesAndVariablesLab/03.ExactSumOfRealNumbers/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+3
+1000000000000000000
+5
+10
+ 
+2
+0.00000000003
+333333333333.3
+ */
+
+namespace Exact_Sum_of_Real_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int numberCount = int.Parse(Console.ReadLine());
+
+            decimal sum = 0;
+
+            for (int i = 0; i < numberCount; i++)
+            {
+                decimal number = decimal.Parse(Console.ReadLine()); 
+
+                sum += number;
+            }
+
+            Console.WriteLine(sum);
+
+        }
+    }
+}
