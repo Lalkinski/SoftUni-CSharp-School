fdff1952158040e88b71eb86cb9f0ef8f37e6925
Lalkinski <156706356+Lalkinski@users.noreply.github.com>
2024-06-25 22:40:09 +0300

    Add files via upload

---
+++ C#Fundamentals/FundametalsExercise/DataTypeAndVariablesExercise/02. Sum Digits/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+ 
+myint.ToString().Length
+
+ */
+
+using System;
+
+namespace _02._Sum_Digits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            int numberCopy = number;
+
+            int sum = 0;
+
+            number = Math.Abs(number);
+
+            foreach (char digitChar in number.ToString())
+            {
+                sum += int.Parse(digitChar.ToString());
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
