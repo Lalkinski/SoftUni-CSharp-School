5ecb7cebbe1906f23e0f9cc0c04d7b037b16535f
Lalkinski <156706356+Lalkinski@users.noreply.github.com>
2024-06-25 22:29:16 +0300

    Add files via upload

---
+++ C#Fundamentals/FundamentalsLab/TextProcessing/04. Text Filter/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _04._Text_Filter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] bannedWords = Console.ReadLine()
+                .Split(", ")
+                .ToArray();
+            string text = Console.ReadLine();
+
+            foreach (var bannedWord in bannedWords)
+            {
+                text = text.Replace(bannedWord, new string('*', bannedWord.Length));
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
---
+++ C#Fundamentals/FundamentalsLab/TextProcessing/05. Digits, Letters and Other/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _05._Digits__Letters_and_Other
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            string letters = string.Empty;
+            string digits = string.Empty;
+            string other = string.Empty;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                char currentChar = text[i];
+                if(char.IsDigit(currentChar))
+                {
+                    digits += currentChar;
+                }
+                else if(char.IsLetter(currentChar))
+                {
+                    letters += currentChar;
+                }
+                else
+                {
+                    other += currentChar;
+                }
+            }
+            Console.WriteLine(digits);
+            Console.WriteLine(letters);
+            Console.WriteLine(other);
+        }
+    }
+}
