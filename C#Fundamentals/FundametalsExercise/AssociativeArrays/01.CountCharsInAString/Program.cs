9822047ac9fc1f9c0e71a17e388f70d84dfe2ed4
Lalkinski <156706356+Lalkinski@users.noreply.github.com>
2024-06-25 22:37:00 +0300

    Add files via upload

---
+++ C#Fundamentals/FundametalsExercise/AssociativeArrays/01.CountCharsInAString/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _1._Count_Chars_in_a_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+
+            Dictionary<char, int> counts = new Dictionary<char, int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char symbol = input[i];
+
+                if(symbol == ' ')
+                {
+                    continue;
+                }
+
+                if (!counts.ContainsKey(symbol))
+                {
+                    counts.Add(symbol, 0);
+                }
+
+                counts[symbol]++;
+            }
+
+            foreach(var output in counts)
+            {
+                char letter = output.Key;
+                int countOfLetter = output.Value;
+
+                Console.WriteLine($"{letter} -> {countOfLetter}");
+            }
+        }
+    }
+}
