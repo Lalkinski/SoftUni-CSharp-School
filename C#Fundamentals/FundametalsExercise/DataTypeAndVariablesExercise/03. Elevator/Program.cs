d526fa5c2306bef411f02fdeb55ed93f11ae4b22
Lalkinski <156706356+Lalkinski@users.noreply.github.com>
2024-06-25 22:41:01 +0300

    Add files via upload

---
+++ C#Fundamentals/FundametalsExercise/DataTypeAndVariablesExercise/03. Elevator/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _03._Elevator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int peopleCount = int.Parse(Console.ReadLine());
+            int capacity = int.Parse(Console.ReadLine());
+
+            int courses = (int)Math.Ceiling((double)peopleCount / capacity);
+
+            Console.WriteLine(courses);
+        }
+    }
+}
