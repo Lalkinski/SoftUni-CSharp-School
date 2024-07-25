13b2c4d3bda744f5ef3a9a93c304ea5fe477ef85
Lalkinski <156706356+Lalkinski@users.noreply.github.com>
2024-07-25 20:17:52 +0100

    Add files via upload

---
+++ C#Fundamentals/C#FundamentalsExercise/RegularExpressionsExercise/02.Race/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public class Example
+{
+    class Racer
+    {
+        public string Name { get; set; }
+    }
+    public static void Main()
+    {
+        string pattern = @"([A-Za-z])";
+        string[] input = Console.ReadLine().Split(", ").ToArray();
+
+        string encryptedMessage;
+        while((encryptedMessage = Console.ReadLine()) != "end of race")
+        {
+            RegexOptions options = RegexOptions.Multiline;
+
+            foreach (Match m in Regex.Matches(encryptedMessage, pattern, options))
+            {
+                Racer racer = new Racer();
+
+            }
+
+        }
+    }
+}
