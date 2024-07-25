34ab5549e41ebfd340ca755e8adef0f6cad9690c
Lalkinski <156706356+Lalkinski@users.noreply.github.com>
2024-07-25 20:11:09 +0100

    Add files via upload

---
+++ C#Fundamentals/C#FundamentalsExercise/ObjectAndClassesExercise/5. Teamwork Projects/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Xml.Linq;
+using static _5._Teamwork_Projects.Program;
+
+namespace _5._Teamwork_Projects
+{
+    internal class Program
+    {
+        public class Person
+        {
+            public string Name { get; set; }
+            public string IdPerson { get; set; }
+            public int Age { get; set; }
+
+             public Person(string name, string id, int age)
+            {
+                Name = name;
+                IdPerson = id;
+                Age = age;
+            }
+        }
+        static void Main(string[] args)
+        {
+            string input;
+
+            List<Person> list = new List<Person>();
+            while((input = Console.ReadLine()) != "End")
+            {
+                string[] person = input.Split();
+                string name = person[0];
+                string id = person[1];  
+                int age = int.Parse(person[2]);
+
+                Person people = new Person(name, id, age);
+
+                list.Add(people);
+            }
+
+            List<Person> sortedPerson = list.OrderBy(x => x.Age).ToList();
+
+            foreach (Person item in sortedPerson)
+            {
+                Console.WriteLine($"{item.Name} with ID: {item.IdPerson} is {item.Age} years old.");
+            }
+        }
+    }
+}
