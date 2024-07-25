aa595245dca698a654da80ed8994238e60ccf448
Lalkinski <156706356+Lalkinski@users.noreply.github.com>
2024-07-25 19:54:56 +0100

    Add files via upload

---
+++ C#Fundamentals/C#FundamentalsExercise/ArraysExercise/01. Train/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+internal class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int total = 0;
+        int[] wagons = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            int passengers = int.Parse(Console.ReadLine());
+            wagons[i] = passengers;
+            total += wagons[i];
+        }
+
+        Console.WriteLine(string.Join(" ", wagons));
+        Console.WriteLine(total);
+    }
+}
---
+++ C#Fundamentals/C#FundamentalsExercise/ArraysExercise/03. Rounding Numbers/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _03._Rounding_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = Console.ReadLine()
+                .Split()
+                .Select(double.Parse)
+                .ToArray();
+
+            foreach (double n in numbers)
+            {
+                Console.WriteLine($"{n} => {(int)Math.Round(n, MidpointRounding.AwayFromZero)}");
+            }
+        }
+    }
+}
