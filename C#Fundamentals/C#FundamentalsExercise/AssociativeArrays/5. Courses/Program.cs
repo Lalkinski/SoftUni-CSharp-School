e3bf3dead182568c43c763a51b8a33a8955b4536
Lalkinski <156706356+Lalkinski@users.noreply.github.com>
2024-07-25 19:48:29 +0100

    Add files via upload

---
+++ C#Fundamentals/C#FundamentalsExercise/AssociativeArrays/5. Courses/Program.cs
@@ -0,0 +1,68 @@
+﻿/*
+Programming Fundamentals : John Smith
+Programming Fundamentals : Linda Johnson
+JS Core : Will Wilson
+Java Advanced : Harrison White
+end
+ */
+using _5._Courses;
+using System.Reflection.Metadata.Ecma335;
+
+namespace _5._Courses
+{
+    public class Course
+    {
+
+        public Course(string name)
+        {
+            Name = name;
+            StudentName = new List<string>();
+        }
+        public string Name { get; set; }
+        public List<string> StudentName { get; set; }
+
+        public override string ToString()
+        {
+            string result = $"{Name}: {StudentName.Count}\n";
+
+            for (int i = 0; i < StudentName.Count; i++)
+            {
+                result += $"-- {StudentName[i]}\n";
+            } 
+            
+            return result.Trim();
+        }
+    }
+ }    
+
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Course> courseMap = new Dictionary<string, Course>();
+
+            string input;
+            while((input = Console.ReadLine()) != "end")
+            {
+                string[] arguments = input.Split(" : ");
+
+                string courseName = arguments[0];   
+                string studentName = arguments[1];
+                
+                if(!courseMap.ContainsKey(courseName))
+                {
+                    Course newCourse = new Course(courseName);
+                    courseMap.Add(courseName, newCourse);
+                }
+
+                courseMap[courseName].StudentName.Add(studentName);
+            }
+
+            foreach(Course course in courseMap.Values)
+            {
+                Console.WriteLine(course);
+            }
+        }
+    }
+
