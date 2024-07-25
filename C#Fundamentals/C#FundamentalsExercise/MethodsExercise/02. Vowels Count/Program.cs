34b4076b7e12538ff1a58f15089c266a62cdcb58
Lalkinski <156706356+Lalkinski@users.noreply.github.com>
2024-07-25 20:04:11 +0100

    Add files via upload

---
+++ C#Fundamentals/C#FundamentalsExercise/MethodsExercise/02. Vowels Count/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace _02._Vowels_Count
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            char[] vowelsChars = text.ToCharArray();
+
+
+            int vowelsSum = vowelsCharsMethod(text, vowelsChars);
+
+            Console.WriteLine(vowelsSum);
+
+        }
+        private static int vowelsCharsMethod(string text, char[] vowelsChars)
+        {
+            int sum = 0;
+            
+            foreach (char letter in vowelsChars)
+            {
+                if (letter == 'a' || letter == 'A')
+                {
+                    sum++;
+                }
+                else if (letter == 'e' || letter == 'E')
+                {
+                    sum++;
+                }
+                else if (letter == 'o' || letter == 'O')
+                {
+                    sum++;
+                }
+                else if (letter == 'i' || letter == 'I')
+                {
+                    sum++;
+                }
+                else if (letter == 'u' || letter == 'U')
+                {
+                    sum++;
+                }
+                else if (letter == 'y' || letter == 'Y')
+                {
+                    sum++;
+                }
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ C#Fundamentals/C#FundamentalsExercise/MethodsExercise/03. Characters in Range/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace _03._Characters_in_Range
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char startSymbol = char.Parse(Console.ReadLine());
+            char endSymbol = char.Parse(Console.ReadLine());
+
+            if (startSymbol > endSymbol)
+            {
+                char swapTemp = startSymbol;
+                startSymbol = endSymbol;
+                endSymbol = swapTemp;
+            }
+
+            printBetweenLetter(startSymbol, endSymbol);
+        }
+
+        private static void printBetweenLetter(char startSymbol, char endSymbol)
+        {
+            for (int i = startSymbol + 1; i < endSymbol; i++)
+            {
+                Console.Write($"{(char)i} ");
+            }
+        }
+    }
+}
+
