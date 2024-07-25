0ec32095d7aac1247f47a7433c0826e1cfdc5ea8
Lalkinski <156706356+Lalkinski@users.noreply.github.com>
2024-07-25 19:57:24 +0100

    Add files via upload

---
+++ C#Fundamentals/C#FundamentalsExercise/ArraysExercise/06. Equal Sum/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace _06._Equal_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int index = -1;
+            for (int i = 0; i < array.Length; i++)
+            {
+
+                index = -1;
+                int rightSum = 0;
+                int leftSum = 0;
+
+                for (int j = i - 1; j >= 0; j--)
+                {
+                    leftSum += array[j];
+                }
+
+                for(int j = i + 1; j < array.Length; j++)
+                {
+                    rightSum += array[j];
+                }
+
+                if(rightSum == leftSum)
+                {
+                    index = i;
+                    break;
+                }
+            }
+
+                if(index != -1)
+                {
+                    Console.WriteLine(index);
+                }
+                else
+                {
+                    Console.WriteLine("no");
+                }
+        }
+    }
+}
---
+++ C#Fundamentals/C#FundamentalsExercise/ArraysExercise/08. Magic Sum/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+ 
+1 7 6 2 19 23
+8
+   
+ */
+
+namespace _08._Magic_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int input = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] + arr[j] == input)
+                    {
+                        Console.WriteLine($"{arr[i]} {arr[j]}");
+
+                    }
+                }
+            }
+        }
+    }
+}
