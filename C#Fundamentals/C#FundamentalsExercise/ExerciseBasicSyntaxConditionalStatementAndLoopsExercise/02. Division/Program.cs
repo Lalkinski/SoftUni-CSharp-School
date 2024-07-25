41bfb16b909bd7f2464bead79419dc9947b54791
Lalkinski <156706356+Lalkinski@users.noreply.github.com>
2024-07-25 20:00:12 +0100

    Add files via upload

---
+++ C#Fundamentals/C#FundamentalsExercise/ExerciseBasicSyntaxConditionalStatementAndLoopsExercise/02. Division/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _02._Division
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            int divisibleNum = 0;
+
+            if (number % 10 == 0)
+            {
+                divisibleNum = 10;
+            }
+            else if (number % 7 == 0)
+            {
+                divisibleNum = 7;
+            }
+            else if(number % 6 == 0)
+            {
+                divisibleNum = 6;
+            }
+            else if(number % 3 == 0)
+            {
+                divisibleNum = 3;
+            }
+            else if(number % 2 == 0)
+            {
+                divisibleNum = 2;
+            }
+
+
+             if(divisibleNum > 0)
+            {
+                Console.WriteLine($"The number is divisible by {divisibleNum}");
+            }
+             else
+            {
+                Console.WriteLine($"Not divisible");
+            }
+            
+        }
+    }
+}
---
+++ C#Fundamentals/C#FundamentalsExercise/ExerciseBasicSyntaxConditionalStatementAndLoopsExercise/03. Vacation/Program.cs
@@ -0,0 +1,81 @@
+﻿namespace _03._Vacation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Input
+            int peopleCount = int.Parse(Console.ReadLine());
+            string typeOfGroup = Console.ReadLine();
+            string dayOfWeek = Console.ReadLine();
+            //Calculation
+            double price = 0;
+            double pricePerPerson = 0;
+
+            if (typeOfGroup == "Students")
+            {
+                if (dayOfWeek == "Friday")
+                {
+                    pricePerPerson = 8.45;
+                }
+                else if (dayOfWeek == "Saturday")
+                {
+                    pricePerPerson = 9.80;
+                }
+                else if (dayOfWeek == "Sunday")
+                {
+                    pricePerPerson = 10.46;
+                }
+                
+                if (peopleCount >= 30)
+                {
+                    pricePerPerson *= 0.85;
+                }
+            }
+            else if (typeOfGroup == "Business")
+            {
+                if (dayOfWeek == "Friday")
+                {
+                    pricePerPerson = 10.90;
+                }
+                else if (dayOfWeek == "Saturday")
+                {
+                    pricePerPerson = 15.60;
+                }
+                else if (dayOfWeek == "Sunday")
+                {
+                    pricePerPerson = 16;
+                }
+                if (peopleCount >= 100)
+                {
+                    peopleCount -= 10;
+                }
+            }
+            else if(typeOfGroup == "Regular")
+            {
+                if (dayOfWeek == "Friday")
+                {
+                    pricePerPerson = 15;
+                }
+                else if (dayOfWeek == "Saturday")
+                {
+                    pricePerPerson = 20;
+                }
+                else if (dayOfWeek == "Sunday")
+                {
+                    pricePerPerson = 22.50;
+                }
+
+                if(peopleCount >= 10 && peopleCount <= 20)
+                {
+                    pricePerPerson *= 0.95;
+                }
+            }
+
+            price = pricePerPerson * peopleCount;
+
+
+            Console.WriteLine($"Total price: {price:f2}");
+        }
+    }
+}
