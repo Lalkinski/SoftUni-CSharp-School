f6cabcb7d9dc835a265ff0299d0441c7d77f2201
Lalkinski <156706356+Lalkinski@users.noreply.github.com>
2024-07-25 20:06:10 +0100

    Add files via upload

---
+++ C#Fundamentals/C#FundamentalsExercise/MethodsExercise/07. NxN Matrix/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _07._NxN_Matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            
+            square(number);
+        }
+
+        private static void square(int number)
+        {
+            for (int i = 0; i < number; i++)
+            {
+                for (int j = 0; j < number; j++)
+                {
+                    Console.Write(number + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ C#Fundamentals/C#FundamentalsExercise/MethodsExercise/9. Palindrome Integers/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _9._Palindrome_Integers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string command;
+            while ((command = Console.ReadLine()) != "END")
+            {
+                bool isPalindrom = isNumberPalindrom(command);
+                Console.WriteLine(isPalindrom);
+            }
+        }
+
+        private static bool isNumberPalindrom(string command)
+        {
+            //121 - 1
+            string firstHalf = command.Substring(0, command.Length / 2);
+
+            //[1, 2, 1]
+            char[] charArr = command.ToCharArray();
+
+            //[1, 2, 1]
+            Array.Reverse(charArr);
+
+            string reversed = new string(charArr);
+
+            //1
+            string lastHalf = reversed.Substring(0, reversed.Length / 2);
+
+            //1 == 1
+            bool isEqual = firstHalf == lastHalf;
+
+            return isEqual;
+
+        }
+    }
+}
