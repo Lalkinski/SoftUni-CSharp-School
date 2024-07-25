a077b4d3f68d913622b4d27bd3bfd41ad14ff6f3
Lalkinski <156706356+Lalkinski@users.noreply.github.com>
2024-07-25 20:02:10 +0100

    Add files via upload

---
+++ C#Fundamentals/C#FundamentalsExercise/ExerciseBasicSyntaxConditionalStatementAndLoopsExercise/07. Vending Machine/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07.Vending_Machine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "";
+            string products = "";
+            decimal coinSum = 0;
+            while ((input = Console.ReadLine()) != "Start")
+            {
+                decimal coins = decimal.Parse(input);
+                if (coins == 0.1m)
+                {
+                    coinSum += coins;
+                }
+                else if (coins == 0.2m)
+                {
+                    coinSum += coins;
+                }
+                else if (coins == 0.5m)
+                {
+                    coinSum += coins;
+                }
+                else if (coins == 1.0m)
+                {
+                    coinSum += coins;
+                }
+                else if (coins == 2.0m)
+                {
+                    coinSum += coins;
+                }
+                else
+                {
+                    Console.WriteLine($"Cannot accept {coins}");
+                }
+
+            }
+            while ((products = Console.ReadLine()) != "End")
+            {
+                if (products == "Nuts")
+                {
+                    if (coinSum < 2.0m)
+                    {
+                        Console.WriteLine("Sorry, not enough money");
+                        continue;
+                    }
+                    coinSum -= 2.0m;
+                    Console.WriteLine($"Purchased {products.ToLower()}");
+
+                }
+                else if (products == "Water")
+                {
+                    if (coinSum < 0.7m)
+                    {
+                        Console.WriteLine("Sorry, not enough money");
+                        continue;
+                    }
+                    coinSum -= 0.7m;
+                    Console.WriteLine($"Purchased {products.ToLower()}");
+
+                }
+                else if (products == "Crisps")
+                {
+                    if (coinSum < 1.5m)
+                    {
+                        Console.WriteLine("Sorry, not enough money");
+                        continue;
+                    }
+                    coinSum -= 1.5m;
+                    Console.WriteLine($"Purchased {products.ToLower()}");
+
+                }
+                else if (products == "Soda")
+                {
+                    if (coinSum < 0.8m)
+                    {
+                        Console.WriteLine("Sorry, not enough money");
+                        continue;
+                    }
+                    coinSum -= 0.8m;
+                    Console.WriteLine($"Purchased {products.ToLower()}");
+
+                }
+                else if (products == "Coke")
+                {
+                    if (coinSum < 1.0m)
+                    {
+                        Console.WriteLine("Sorry, not enough money");
+                        continue;
+                    }
+                    coinSum -= 1.0m;
+                    Console.WriteLine($"Purchased {products.ToLower()}");
+
+                }
+                else
+                {
+                    Console.WriteLine("Invalid product");
+                }
+            }
+            Console.WriteLine($"Change: {coinSum:F2}");
+
+        }
+    }
+}
+
---
+++ C#Fundamentals/C#FundamentalsExercise/ExerciseBasicSyntaxConditionalStatementAndLoopsExercise/09. Padawan Equipment/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+using System;
+
+
+internal class Program
+{
+
+    static void Main()
+    {
+        double moneyAmount = double.Parse(Console.ReadLine());
+        int studentsCount = int.Parse(Console.ReadLine());
+        double saberPrice = double.Parse(Console.ReadLine());
+        double robePrice = double.Parse(Console.ReadLine());
+        double beltPrice = double.Parse(Console.ReadLine());
+
+        double percent = (double)studentsCount * 10 / 100;
+
+        double allSabers = studentsCount + Math.Ceiling(percent);
+        double sabers = saberPrice * allSabers;
+        double robes = robePrice * studentsCount;
+        double beltDiss = studentsCount - Math.Ceiling((double)(studentsCount / 6)); double belts = beltPrice * beltDiss;
+        double total = sabers + robes + belts;
+        Console.WriteLine(total <= moneyAmount ?
+        $"The money is enough - it would cost {total:F2}lv." :
+        $"John will need {total - moneyAmount:F2}lv more.");
+    }
+}
