aa1d602ac862d44a3631b5845995283fe1df79a4
Lalkinski <156706356+Lalkinski@users.noreply.github.com>
2024-07-25 20:20:11 +0100

    Add files via upload

---
+++ C#Fundamentals/C#FundamentalsExercise/TextProcessingExercise/06. Replace Repeating Chars/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+
+namespace _06._Replace_Repeating_Chars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            StringBuilder result = new StringBuilder(capacity: text.Length);
+
+            int index = 0;
+            while (index < text.Length)
+            {
+                while(index + 1 < text.Length && text[index] == text[index + 1]) index++;
+
+
+                result.Append(text[index]);
+                index++;
+            }
+
+            Console.WriteLine(result.ToString());
+        }
+    }
+}
