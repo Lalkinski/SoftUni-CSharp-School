a68380210413c704de5beac564a1f43a07ca81fe
Lalkinski <156706356+Lalkinski@users.noreply.github.com>
2024-07-29 10:50:20 +0100

    Add files via upload

---
+++ C#OOP/C#OOP-ExamPreparation/C# OOP Retake Exam – 19 December 2023/Models/OxygenClimber.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HighwayToPeak.Models
+{
+    public class OxygenClimber : Climber
+    {
+        private const int initialStamina = 10;
+        public OxygenClimber(string name) 
+            : base(name, initialStamina)
+        {
+        }
+
+        public override void Rest(int daysCount)
+        {
+            this.Stamina += daysCount;
+        }
+    }
+}
